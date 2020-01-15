4b528aa8d3dfe93f25199ea76ed3652a71a676af
alsubi <alsubaie22145@gmail.com>
2020-01-15 16:44:22 +0300

    Updates

---
+++ precache-manifest.b0f6937c78bc2e90decd67276a57fd74.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f3848efe8de57a7eedb40801ebca6d3",
+    "url": "/project-3/index.html"
+  },
+  {
+    "revision": "7e753ba2a5ad66876871",
+    "url": "/project-3/static/css/2.b5b542c0.chunk.css"
+  },
+  {
+    "revision": "3769ba8b03bc4c913af9",
+    "url": "/project-3/static/css/main.17eb934e.chunk.css"
+  },
+  {
+    "revision": "7e753ba2a5ad66876871",
+    "url": "/project-3/static/js/2.7ae0fb13.chunk.js"
+  },
+  {
+    "revision": "182a7341d888ceed9c5ba961b259f080",
+    "url": "/project-3/static/js/2.7ae0fb13.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3769ba8b03bc4c913af9",
+    "url": "/project-3/static/js/main.6c3ad534.chunk.js"
+  },
+  {
+    "revision": "e145e9d0bf553d0388c2",
+    "url": "/project-3/static/js/runtime-main.0fa09ced.js"
+  },
+  {
+    "revision": "04eb8fc57f27498e5ae37523e3bfb2c7",
+    "url": "/project-3/static/media/revicons.04eb8fc5.woff"
+  },
+  {
+    "revision": "17629a5dfe0d3c3946cf401e1895f091",
+    "url": "/project-3/static/media/revicons.17629a5d.ttf"
+  },
+  {
+    "revision": "2feb69ccb596730c72920c6ba3e37ef8",
+    "url": "/project-3/static/media/revicons.2feb69cc.eot"
+  }
+]);
